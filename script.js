a31b46aa70f17b19a8391bb2bfed93906feae2bc
Andrew Wiskus <MyWork@Andrews-MacBook-Pro.local>
2016-07-28 09:27:00 -0500

    adding js mod

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 var locations = []; // this array will hold your objects
 
 // object constructor
+function State(state, locationCount, revenue) {
+	this.state = state;
+	this.revenue = revenue;
+	this.locationCount = locationCount;
 
+	this.averageRevenue = function() {
+		
+		return revenue / locationCount;
+}
+
+}
 // object instances
-
+var IL = new State("IL", 12, 5000);
+var MN = new State("MN", 3, 300);
+var NV = new State("NV", 1, 25000);
 // push object instances to locations array
+locations.push(IL);
+locations.push(MN);
+locations.push(NV);
+
+console.log(MN);
 
 // DO NOT MODIFY THIS CODE
 $(document).ready(function () {
